edd4f396afcdecee47e21422d83a47ca7477b248
Вася Пупкин <BomBimBom@bk.ru>
2021-05-17 23:40:57 +0300

    закончили оплату

--- Merchant/Paypal/PDTHolder.cs
+++ Merchant/Paypal/PDTHolder.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace BlazorStore.Merchant.Paypal
 {
@@ -13,7 +15,7 @@ namespace BlazorStore.Merchant.Paypal
         public int InvoiceNumber { get; set; }
         public string PaymentStatus { get; set; }
         public string PayerFirstName { get; set; }
-        public double PaymentFree { get; set; }
+        public double PaymentFee { get; set; }
         public string BusinesEmail { get; set; }
         public string PayerEmail { get; set; }
         public string TxToken { get; set; }
@@ -26,13 +28,13 @@ namespace BlazorStore.Merchant.Paypal
         public string Custom { get; set; }
         private static string authToken, txToken, query, strResponse;
 
-        public PDTHolder Success(string txt)
+        public static PDTHolder Success(string txt)
         {
             var payPalConfig = Paypal.GetPayPalConfig();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             authToken = payPalConfig.AuthToken;
             txToken = txt;
-            query = string.Format($"cmd=_notify-sync&tx={txToken}&at={authToken}");
+            query = string.Format($"cmd=_notify-synch&tx={txToken}&at={authToken}");
             var url = payPalConfig.PostUrl;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -66,12 +68,67 @@ namespace BlazorStore.Merchant.Paypal
             {
                 string[] stringArray = postData.Split('\n');
                 int i;
+
+                for (i = 1; i < stringArray.Length - 1; i++)
+                {
+                    var array1 = stringArray[i].Split('=');
+                    sKey = array1[0];
+                    sValue = HttpUtility.UrlDecode(array1[1]);
+                    switch (sKey)
+                    {
+                        case "mc_gross":
+                            ph.GrossTotal = double.Parse(sValue, CultureInfo.InvariantCulture);
+                            break;
+                        case "invoice":
+                            ph.InvoiceNumber = Convert.ToInt32(sValue);
+                            break;
+                        case "payment_status":
+                            ph.PaymentStatus = sValue;
+                            break;
+                        case "first_name":
+                            ph.PayerFirstName = sValue;
+                            break;
+                        case "mc_fee":
+                            ph.PaymentFee = Convert.ToInt32(sValue);
+                            break;
+                        case "business":
+                            ph.BusinesEmail = sValue;
+                            break;
+                        case "payer_email":
+                            ph.PayerEmail = sValue;
+                            break;
+                        case "Tx_Token":
+                            ph.TxToken = sValue;
+                            break;
+                        case "last_name":
+                            ph.PayerLastName = sValue;
+                            break;
+                        case "receiver_email":
+                            ph.ReceiverEmail = sValue;
+                            break;
+                        case "item_name":
+                            ph.ItemName = sValue;
+                            break;
+                        case "mc_currency":
+                            ph.Currency = sValue;
+                            break;
+                        case "txn_id":
+                            ph.TransactionId = sValue;
+                            break;
+                        case "custom":
+                            ph.Custom = sValue;
+                            break;
+                        case "subscr_id":
+                            ph.SubscriberId = sValue;
+                            break;
+                    }
+                }
+
                 return ph;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                throw new ArgumentException(ex.Message);
             }
         }
     }
